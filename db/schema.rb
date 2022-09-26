0e138cde509d66adcf0e272de148bd888b5fc563
rahulgupta34 <rahul.g@simformsolutions.com>
2022-09-26 10:00:58 +0530

    create customer and order model for sql joins practice [26/09/2022]

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_111233) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_111323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
-    t.date "year_published"
-    t.boolean "out_of_print"
-    t.decimal "cost", precision: 8, scale: 2
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "status"
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -31,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_111233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "amount"
+    t.integer "customer"
   end
 
   create_table "users", force: :cascade do |t|
